d68ed6ac9523d500bfab0086b41ab82a1b39d372
Jayaprakash sivagami <jayaprakashsivagami@Jps-Mac.local>
2021-02-26 08:10:37 +0530

    added Dockerfile

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 //client side code
 
-// console.log('client side java script loaded!!')
+console.log('client side java script loaded!!')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
